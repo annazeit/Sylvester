27572111e74d39c5ed1bd9082d83bddfd9525544
annazeit <77589107+annazeit@users.noreply.github.com>
2024-07-27 20:59:26 +0100

    tail added to the head

--- src/snake.rs
+++ src/snake.rs
@@ -1,8 +1,11 @@
 use bevy::app::{App, Plugin, Startup, Update};
 use bevy::color::palettes::basic::YELLOW;
+use bevy::color::palettes::css::BLUE;
 use bevy::input::ButtonInput;
 use bevy::math::Vec2;
-use bevy::prelude::{Commands, Component, Gizmos, KeyCode, Query, Res};
+use bevy::prelude::{Commands, Component, Gizmos, KeyCode, Mut, Query, Res};
+use std::f32::*;
+use std::mem::zeroed;
 
 pub struct SnakePlugin;
 impl Plugin for SnakePlugin {
@@ -13,9 +16,9 @@ impl Plugin for SnakePlugin {
 }
 fn snake_start(mut commands: Commands) {
     for i in 0..1 {
-        commands.spawn(Snake {
+        commands.spawn(SnakeHead {
             name: format!("snake #{i}"),
-            head_pos: Vec2::new(i as f32 * 20.0, 0.0),
+            head_pos: Vec2::new(0.0, i as f32 * -100.0),
             head_direction_angle: 0.0,
             distance_from_last_turn: 0.0,
             direction_changes: vec![],
@@ -25,7 +28,7 @@ fn snake_start(mut commands: Commands) {
     }}
 
 #[derive(Component)]
-struct Snake {
+struct SnakeHead {
     name: String,
     head_pos: Vec2,
     head_direction_angle: f32,
@@ -36,6 +39,7 @@ struct Snake {
     //rotation speed in degrees per second. this value defines how quickly the object changes direction
     rotation_speed_in_degrees: f32
 }
+
 #[derive(Component)]
 struct DirectionChange {
     old_direction_angle: f32,
@@ -44,11 +48,12 @@ struct DirectionChange {
 
 fn snake_update(
     mut gizmos: Gizmos,
-    mut snake_query: Query<&mut Snake>,
+    mut snake_query: Query<&mut SnakeHead>,
     keyboard_input: Res<ButtonInput<KeyCode>>
 ) {
     for mut snake in &mut snake_query {
-        let head_radius = 50.0;
+        let head_radius = 25.0;
+
 
         let movement: f32 = {
             let unit = {
@@ -59,16 +64,27 @@ fn snake_update(
             unit * snake.movement_speed
         };
         let rotation: f32 = {
-            if keyboard_input.pressed(KeyCode::ArrowRight) { 1.0 }
+            if keyboard_input.pressed(KeyCode::ArrowRight) {
+                1.0
+                }
             else if keyboard_input.pressed(KeyCode::ArrowLeft) { -1.0 }
             else { 0.0 }
         };
 
-        snake.head_direction_angle += std::f32::consts::PI / 180.0 * snake.rotation_speed_in_degrees * rotation;
-        let x = f32::sin(snake.head_direction_angle) * movement;
-        let y = f32::cos(snake.head_direction_angle) * movement;
-        snake.head_pos += Vec2::new(x, y);
+        snake.head_direction_angle += consts::PI / 180.0 * snake.rotation_speed_in_degrees * rotation;
+        let x_head = f32::sin(snake.head_direction_angle) * movement;
+        let y_head = f32::cos(snake.head_direction_angle) * movement;
+        snake.head_pos += Vec2::new(x_head, y_head);
 
         gizmos.circle_2d(snake.head_pos, head_radius, YELLOW);
+
+        let distance = head_radius * 2.0;
+        let x_tail = f32::sin(snake.head_direction_angle - consts::PI) * distance;
+        let y_tail = f32::cos(snake.head_direction_angle - consts::PI) * distance;
+        let tail_pos = snake.head_pos + Vec2::new(x_tail, y_tail);
+
+
+        gizmos.circle_2d(tail_pos, 15.0, BLUE);
+
     }
 }
