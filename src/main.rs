e0fac7aa1d5f6e52b1dfc9d00672b2fa307026e1
annazeit <77589107+annazeit@users.noreply.github.com>
2024-07-27 12:20:13 +0100

    snake moves using arrow keys

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ use bevy::color::palettes::css::*;
 use bevy::input::keyboard::keyboard_input_system;
 use crate::grid::*;
 use crate::sprite::*;
-use std::vec::Vec;
 
 fn main() {
     App::new()
@@ -37,6 +36,7 @@ struct Sprite {
 
 #[derive(Component)]
 struct Snake {
+    name: String,
     head_pos: Vec2,
     head_direction_angle: f32,
     distance_from_last_turn: f32,
@@ -54,11 +54,24 @@ struct DirectionChange {
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+
     for i in 0..3
     {
         commands.spawn(Sprite {
             position: Vec2::new(i as f32 * 100.0 - 400., -100.0),
             direction: Direction::Up,
+        });
+    }
+
+    for i in 0..2 {
+        commands.spawn(Snake {
+            name: format!("snake #{i}"),
+            head_pos: Vec2::new(i as f32 * 20.0, 0.0),
+            head_direction_angle: 0.0,
+            distance_from_last_turn: 0.0,
+            direction_changes: vec![],
+            movement_speed: 0.0,
+            rotation_speed: 0.0,
         });
     }
 }
@@ -68,19 +81,16 @@ fn snake(
     mut snake_query: Query<&mut Snake>,
     keyboard_input: Res<ButtonInput<KeyCode>>
 ) {
-
     for mut snake in &mut snake_query {
         let head_radius = 50.0;
-        snake.head_pos = Vec2::new(0.0, 0.0);
-        snake.head_direction_angle = 0.0;
 
         if keyboard_input.pressed(KeyCode::ArrowRight) {
-            snake.head_direction_angle += 10.0;
+            snake.head_pos += Vec2::new(10.0, 0.0);
         }
         if keyboard_input.pressed(KeyCode::ArrowLeft) {
-            snake.head_direction_angle -= 10.0;
+            snake.head_pos += Vec2::new(-10.0, 0.0);
         }
-        gizmos.circle_2d(Vec2::new(0.0, 0.0), head_radius, YELLOW);
+        gizmos.circle_2d(snake.head_pos, head_radius, YELLOW);
         //snake.head_pos += Vec2::new(0.0, 10.0);
     }
 }
